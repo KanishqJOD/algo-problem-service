05fe0525425ac2a2e2109b47bc4171cb22f210d2
kanishq24 <kanishqqtahalyanii@gmail.com>
2025-06-19 18:22:28 +0530

    Feat: Added routing structure for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/server.config');
+
+const apiRouter = require('./routes');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req,res) => {
      return res.json({message: 'Problem service is alive'});
@@ -13,4 +18,4 @@ app.get('/ping', (req,res) => {
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
-});
+}); 
