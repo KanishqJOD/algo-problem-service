a84585c071ac9c2769df5c8fdfd86dffcbd5db41
kanishq24 <kanishqqtahalyanii@gmail.com>
2025-06-20 14:11:40 +0530

    Feat: Added error Handler middle ware with base error class stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Code problem setting service
 
+
+
+## How routing is working in the project
+
+     -/api/v1/problems/ping
+       - because the route starts with /api
+        /api            -> /v1    -> /problems                  -> /ping
+        apiRouter -> v1Router -> problemRouter -> problemController
+
+
+// last middleware controller, it will collect the request and send it to the service layer, express js is unopinated framework, so for meaning and sense
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/server.config');
+const BaseError = require('./errors/Base.error');
 
 const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -12,10 +14,43 @@ app.use(bodyParser.text());
 
 app.use('/api', apiRouter);
 
+// If any request comes and route start with /api, we map it to apiRouter
+
 app.get('/ping', (req,res) => {
      return res.json({message: 'Problem service is alive'});
 });
 
+// last middleware
+app.use(errorHandler);
+
 app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
+    //    try {
+    //        throw new BaseError("Not Found", 404, "Resource not found", {});   
+    //       // 1. opened a db connection
+    //       // 2. queried on db, but u wrong syntax query
+    //       // 3. Exception will be thrown
+    //    } catch(error) {
+    //     // log the error
+    //     console.log("something went error",error.name,);  // exception handling
+    //    } finally {
+    //     // close the db connection
+    //     console.log("executed finally")  // always happen
+    //    }
 }); 
+
+// error or exception handling is imp because the thing is that if we can handle exception then program get stop
+
+// example 
+
+/*
+     async function fun() {
+        try {
+        await fetch("http://localhost:3002");
+        } catch(error) {
+            console.log("something went wrong", error);
+        }
+     }
+
+     func()  
+*/
